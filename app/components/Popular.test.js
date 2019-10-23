8a9622380229183fc6184adc7ce46e42bd1e3ec1
Karina Thomas <karina.thomas@verint.com>
2019-10-23 12:22:15 +0100

    Adding a snapshot test for Popular class

--- app/components/Popular.test.js
+++ app/components/Popular.test.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import {shallow} from 'enzyme'
+import renderer from 'react-test-renderer';
 
 import Popular from "./Popular";
 
 describe('Popular', () => {
+    let wrapper;
+    beforeAll(() => {
+        wrapper = shallow(<Popular/>);
+    });
     it('does not crash on render', () => {
-        let wrapper = shallow(<Popular/>);
         expect(wrapper.find('ul')).not.toBeNull();
+    });
+    it('renders correctly', () => {
+        expect(renderer.create(<Popular/>).toJSON()).toMatchSnapshot();
     });
 });
